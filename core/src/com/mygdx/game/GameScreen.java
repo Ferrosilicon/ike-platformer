b692eafc733fa234f7b435046856e20afb180f6f
ShubhangD <shubhang.desai@gmail.com>
2015-12-27 16:41:46 -0600

    add movement system
    added movement systems to the game

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,5 +1,8 @@
 package com.mygdx.game;
 
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
@@ -11,6 +14,9 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.mygdx.game.components.MovementComponent;
+import com.mygdx.game.components.PositionComponent;
+import com.mygdx.game.systems.MovementSystem;
 
 public class GameScreen extends ScreenAdapter {
 
@@ -30,9 +36,17 @@ public class GameScreen extends ScreenAdapter {
     boolean jumping = false;
     float deltaT = 0;
 
+    PooledEngine engine = new PooledEngine(1,
+            100,
+            0,
+            5);
+    Entity mainCharacter;
+    MovementComponent movementComponent;
+    PositionComponent positionComponent;
+
     public GameScreen(MyGdxGame game) {
         this.game = game;
-
+        engine.addSystem(new MovementSystem());
         initiatePlayer();
         initiateTiledMap();
         initiateCamera();
@@ -40,9 +54,18 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private void initiatePlayer() {
-        playerX = 0;
-        playerY = 4;
-        playerTexture = new Texture(Gdx.files.internal("player.png"));
+//        playerX = 0;
+//        playerY = 4;
+//        playerTexture = new Texture(Gdx.files.internal("player.png"));
+        mainCharacter = engine.createEntity();
+
+        mainCharacter.add(new PositionComponent());
+        positionComponent = mainCharacter.getComponent(PositionComponent.class);
+        positionComponent.position.add(0, 4);
+
+        mainCharacter.add(new MovementComponent());
+        movementComponent = mainCharacter.getComponent(MovementComponent.class);
+        movementComponent.acceleration.add(0, -16);
     }
 
     private void initiateTiledMap() {
@@ -63,33 +86,41 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void render(final float delta) {
-        updateMovement(delta);
+        updateVectors(delta);
+        engine.update(delta);
         updateCamera();
         updateRender();
     }
 
-    private void updateMovement(final float delta) {
+    private void updateVectors(final float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.D))
-            playerX += UNITS_SECOND * delta;
-        if (Gdx.input.isKeyPressed(Input.Keys.A))
-            playerX -= UNITS_SECOND * delta;
-        if (Gdx.input.isKeyPressed(Input.Keys.W))
-            jumping = true;
+            movementComponent.velocity.set(7,0);
+        else if (Gdx.input.isKeyPressed(Input.Keys.A))
+            movementComponent.velocity.set(-7,0);
+        else
+            movementComponent.velocity.set(0, 0);
 
-        if (jumping) {
-            deltaT += delta;
-            playerY = INITIAL_JUMP_VELOCITY * deltaT - GRAVITATIONAL_ACCELERATION * deltaT * deltaT + 4;
-            if (playerY < 4) {
-                jumping = false;
-                deltaT = 0;
-                playerY = 4;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            movementComponent.velocity.add(0, 16);
+            movementComponent.jumping = true;
         }
+//        if (Gdx.input.isKeyPressed(Input.Keys.W))
+//            jumping = true;
+//
+//        if (jumping) {
+//            deltaT += delta;
+//            playerY = INITIAL_JUMP_VELOCITY * deltaT - GRAVITATIONAL_ACCELERATION * deltaT * deltaT + 4;
+//            if (playerY < 4) {
+//                jumping = false;
+//                deltaT = 0;
+//                playerY = 4;
+//            }
+//        }
 
-        if (playerX < 0)
-            playerX = 0;
-        else if (playerX > mapWidth - 1)
-            playerX = mapWidth - 1;
+//        if (playerX < 0)
+//            playerX = 0;
+//        else if (playerX > mapWidth - 1)
+//            playerX = mapWidth - 1;
     }
 
     private void updateCamera() {
