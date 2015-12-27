a5161b690bf8aee2c314483e793bb60158e9c0ab
Ryan Greene <rabrg96@gmail.com>
2015-12-27 13:23:20 -0600

    Fixed launchers and added Ashley library

--- html/src/com/mygdx/game/client/HtmlLauncher.java
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.mygdx.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.GameScreen;
 import com.mygdx.game.MyGdxGame;
 
 public class HtmlLauncher extends GwtApplication {
