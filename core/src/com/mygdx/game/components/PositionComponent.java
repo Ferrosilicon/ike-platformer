cb96092bf2c7ee89f79f4b39add00d02bffe198b
Ryan Greene <rabrg96@gmail.com>
2015-12-27 13:49:42 -0600

    Three core components

---
+++ core/src/com/mygdx/game/components/PositionComponent.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+public class PositionComponent implements Component {
+    public Vector2 position = new Vector2();
+}
