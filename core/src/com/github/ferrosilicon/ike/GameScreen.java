81d431126fa2307e4da60ab8102941fa6f8c511b
Ryan Greene <rabrg96@gmail.com>
2016-01-18 15:08:52 -0600

    Made movement slightly less broken

--- core/src/com/github/ferrosilicon/ike/GameScreen.java
+++ core/src/com/github/ferrosilicon/ike/GameScreen.java
@@ -7,12 +7,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.github.ferrosilicon.ike.model.Level;
 import com.github.ferrosilicon.ike.model.WorldManager;
 
 public final class GameScreen extends ScreenAdapter {
 
-    private static final int MAX_VELOCITY = 3;
+    private static final Vector2 MAX_VELOCITY = new Vector2(3, 5);
 
     private final IkeGame game;
 
@@ -42,23 +43,25 @@ public final class GameScreen extends ScreenAdapter {
         camera.position.x = MathUtils.clamp(worldManager.player.getPosition().x,
                 camera.viewportWidth / 2f, level.mapWidth - (camera.viewportWidth / 2f));
         level.render(camera);
-        Vector2 vel = worldManager.player.getLinearVelocity();
-        Vector2 pos = worldManager.player.getPosition();
 
-// apply left impulse, but only if max velocity is not reached yet
-        if (Gdx.input.isKeyPressed(Input.Keys.A) && vel.x > -MAX_VELOCITY) {
-            worldManager.player.applyLinearImpulse(-0.80f, 0, pos.x, pos.y, true);
-        }
-
-// apply right impulse, but only if max velocity is not reached yet
-        if (Gdx.input.isKeyPressed(Input.Keys.D) && vel.x < MAX_VELOCITY) {
-            worldManager.player.applyLinearImpulse(0.80f, 0, pos.x, pos.y, true);
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.W) && vel.x < MAX_VELOCITY) {
-            worldManager.player.applyLinearImpulse(0, 0.80f, pos.x, pos.y, true);
-        }
+        updateInput();
         worldManager.step(deltaTime, camera);
+    }
+
+    private void updateInput() {
+        final Body player = worldManager.player;
+        final Vector2 vel = player.getLinearVelocity();
+        final Vector2 pos = player.getPosition();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A) && vel.x > -MAX_VELOCITY.x) {
+            player.applyLinearImpulse(-0.80f, 0, pos.x, pos.y, true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D) && vel.x < MAX_VELOCITY.x) {
+            player.applyLinearImpulse(0.80f, 0, pos.x, pos.y, true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W) && vel.y < MAX_VELOCITY.y) {
+            player.applyLinearImpulse(0, 0.80f, pos.x, pos.y, true);
+        }
     }
 
     @Override
